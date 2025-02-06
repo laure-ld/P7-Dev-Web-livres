79ec0723984cd1499e6f5247404601c22790215e
Laure <laure_ld@outlook.fr>
2025-02-06 23:06:39 +0100

    Schéma des objects book et user

--- backend/models/Books.js
+++ backend/models/Books.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
-const bookSchema = mongoose.Schema({
+const bookSchema = new mongoose.Schema({
+  userId : { type: String, required: true },
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  author : { type: String, required: true },
+  year: { type: Number, required: true },
   imageUrl: { type: String, required: true },
-  userId: { type: String, required: true },
+  genre: { type: String, required: true },
+  rating : [
+    {
+        userId : { type: String, required: true},
+        grade : { type: Number, required: true},
+    }
+  ],
+  averageRating : { type: Number },
 });
 
-module.exports = mongoose.model('Books', thingSchema);
+module.exports = mongoose.model('Book', bookSchema);
